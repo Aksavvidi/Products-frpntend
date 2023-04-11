50554b551e1f9f5941b276baa9a3af5b382bc14f
Aksavvidi <aksavvidi@gmail.com>
2023-04-11 19:25:29 +0300

    user-list

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { UserAPIList } from './user.interfaces';
 
 const USER_API = 'https://codingfactory.ddns.net/api/user'
 
@@ -9,7 +10,7 @@ export class UserService {
   constructor(private http: HttpClient) {}
 
   findAll(){
-    return this.http.get(`${USER_API}/findAll`)
+    return this.http.get<UserAPIList>(`${USER_API}/findAll`)
   }
 }
 
--- src/app/user/users-list/users-list.component.ts
+++ src/app/user/users-list/users-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
+import { UserAPIList } from '../user.interfaces';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrls: ['./users-list.component.css']
 })
-export class UsersListComponent {
+export class UsersListComponent implements OnInit {
+  constructor(private userService: UserService) {}
+
+  loading = false;
+
+  ngOnInit(): void {
+    console.log('Starting "findAll" API call');
+    this.loading = true;
+    this.userService.findAll().subscribe({
+      next: (apiData: UserAPIList) => {
+        const{status, data } = apiData;
+        console.log(status, data);
+      },
+      error: (error) => {
+        this.loading = false;
+        console.log(error)
+      },
+      complete: () => {
+        this.loading = false;
+        console.log('API call completed')
+      },
+    })
+  }
 
 }
