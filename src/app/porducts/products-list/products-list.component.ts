43644bcbd46238357e138d7e0894554abaa51131
Aksavvidi <aksavvidi@gmail.com>
2023-04-24 14:40:28 +0300

    all

--- src/app/porducts/products-list/products-list.component.ts
+++ src/app/porducts/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../products.service';
+import { ProductsAPIList } from '../products.interfaces';
 
 @Component({
   selector: 'app-products-list',
@@ -6,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products-list.component.css']
 })
 export class ProductsListComponent implements OnInit {
+  constructor(private productsService: ProductsService) {}
+
+  loading = false;
+
   ngOnInit(): void {
-      throw new Error ('Method not implemented.')
+      console.log("Starting 'findAll' API call");
+      this.loading = true;
+      this.productsService.findAll().subscribe({
+        next: (apiProData: ProductsAPIList) => {
+          const {status, data} = apiProData
+          console.log(status, data);
+        },
+        error: (error) => { 
+          this.loading = false;
+          console.log(error)},
+        complete: () => {
+          this.loading = false;
+          console.log('API call completed');
+        },
+      });
   }
-
-
 }
