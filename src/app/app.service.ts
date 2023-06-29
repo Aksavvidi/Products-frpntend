99254e16cf578f34974ca731381705ea92c1e120
Aksavvidi <aksavvidi@gmail.com>
2023-06-29 16:21:40 +0300

    styartstyling

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {User,  UserAPIUserOne } from 'shared';
+import { UserAPIUserOne } from 'shared';
 import { BehaviorSubject } from 'rxjs';
 import { UiService } from 'ui';
 import { Router } from '@angular/router';
